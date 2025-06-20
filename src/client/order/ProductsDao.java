0139c3b1580d1f07a36d5b4ee0ebb7e47e494cae
wlsksekf <jinaniyagi@gmail.com>
2025-06-20 14:46:13 +0900

    vo remove, categorypanel
    6개였던 vo 하나 삭제해서 5개로 변경
    
    오더패널, 카테고리패널, 메뉴패널, 메뉴버튼, 카트패널 ui 제작
    
    카테고리패널, orders.xml, ProductsDao 이 3개 수정해 db에서 카테고리 가져오도록 수정

---
+++ src/client/order/ProductsDao.java
@@ -0,0 +1,24 @@
+package client.order;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import java.util.List;
+
+public class ProductsDao {
+
+    private SqlSessionFactory factory;
+
+    public ProductsDao(SqlSessionFactory factory) {
+        this.factory = factory;
+    }
+
+    public List<String> selectDistinctCategories() {
+        List<String> categories = null;
+        try (SqlSession session = factory.openSession()) {
+            categories = session.selectList("orders.selectDistinctCategories");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return categories;
+    }
+}
