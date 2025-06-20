d01a4f4f17c80fdba5d39922583f994f14bd46d3
Taein Jang <80192692+roblej@users.noreply.github.com>
2025-06-20 16:51:39 +0900

    ImageUpload

--- src/client/admin/CouponManagerDialog.java
+++ src/client/admin/CouponManagerDialog.java
@@ -14,12 +14,16 @@ public class CouponManagerDialog extends JDialog {
     int i;
 
     public CouponManagerDialog(MainFrame f, CouponManagerPanel p, boolean modal, CouponVO vo, List<CouponVO> couponList, int i) {
+        super(f,modal);
         this.f = f;
         this.vo = vo;
         this.couponList = couponList;
         this.i = i;
         this.p = p;
-
-
+        ImageUploadButton btn = new ImageUploadButton("이미지 업로드",this);
+        this.add(btn);
+        setTitle("Update Coupon");
+        setSize(400, 300);
+        setLocationRelativeTo(f); // 부모 프레임 중앙에 위치
     }
 }
